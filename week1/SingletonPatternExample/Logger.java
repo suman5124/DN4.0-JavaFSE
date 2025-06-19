b355f584db4312a3f49e2a031ebb066e697d3c4c
Suman <suman@Suman-ka-MacBook-Air.local>
2025-06-20 01:22:04 +0530

    week 1 design and pattern ex 1

---
+++ week1/SingletonPatternExample/Logger.java
@@ -0,0 +1,19 @@
+package week1.SingletonPatternExample;
+
+public class Logger {
+    private static Logger instance;
+
+    private Logger() {
+    }
+
+    public static Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void log(String message) {
+        System.out.println("Log: " + message);
+    }
+}
